cebd2e5ae151c617927c54fee5773c6f361fdce0
Josef Eisl <josef.eisl@oracle.com>
2024-09-19 11:45:06 +0200

    better message

--- src/module2/com/example/Main.java
+++ src/module2/com/example/Main.java
@@ -3,7 +3,7 @@ package com.example;
 public class Main {
   public static void main(String[] args) {
     System.out.println(Main.class.getModule());
-    System.out.println("Hello com.example.Main");
+    System.out.println("Hello otherexample/com.example.Main");
 	try {
 		System.load("bla");
 	} catch (Throwable e) {
